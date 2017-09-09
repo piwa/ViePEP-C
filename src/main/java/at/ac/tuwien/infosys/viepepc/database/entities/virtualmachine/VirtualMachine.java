ea176f6906fa4cd1f48f1b40f1d9e99648e37a41
Philipp Waibel <p.waibel@infosys.tuwien.ac.at>
2017-09-09 18:29:57 +0200

    Improved watchdog

--- src/main/java/at/ac/tuwien/infosys/viepepc/database/entities/virtualmachine/VirtualMachine.java
+++ src/main/java/at/ac/tuwien/infosys/viepepc/database/entities/virtualmachine/VirtualMachine.java
@@ -131,6 +131,7 @@ public class VirtualMachine implements Serializable {
                 ", startedAt=" + startString +
                 ", terminateAt=" + toBeTerminatedAtString +
                 ", location=" + location +
+                ", googleName=" + getGoogleName() + 
                 ", ip adress=" + ipAddress +
                 '}';
     }
